e53db59391d4df440a81f36667b03cd2df6f84d8
iosDevelopForYou <iosdevelopforyou@gmail.com>
2023-02-26 19:28:12 +0400

    homework 2.1 is complete

---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Marat Guseynov on 16.02.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    
+    private let profileHeaderView = ProfileHeaderView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profileHeaderView.setupView()
+        view.addSubview(profileHeaderView)
+        
+        configure()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        profileHeaderView.frame = view.frame
+    }
+    
+    
+    
+    func configure() {
+        view.backgroundColor = .lightGray
+        navigationItem.title = "Profile"
+    }
+}
+
